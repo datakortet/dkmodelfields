3883f63aa0ede2489d5d6acd6681e4ac5facd551
thebjorn <bp@datakortet.no>
2019-08-31 15:34:17 +0200

    fix find packages

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@
 """dkmodelfields - Django model fields
 """
 
-import sys
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
+import io, setuptools
 
-version = '0.1.0'
+version = '1.0.1'
 
 classifiers = """\
 Development Status :: 3 - Alpha
@@ -20,37 +18,21 @@ Topic :: Software Development :: Libraries
 """
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
-setup(
+setuptools.setup(
     name='dkmodelfields',
+
     version=version,
-    requires=[],
+    maintainer="Norsk Test",
+    maintainer_email="itdrift@norsktest.no",
+    include_package_data=True,
+    packages=setuptools.find_packages(exclude=['tests']),
+    description=__doc__.strip(),
+    classifiers=[line for line in classifiers.split('\n') if line],
+    long_description=io.open('README.rst', encoding='utf-8').read(),
+
     install_requires=[
         'dk',
         'Django',
     ],
-    description=__doc__.strip(),
-    classifiers=[line for line in classifiers.split('\n') if line],
-    long_description=open('README.rst').read(),
-    cmdclass={'test': PyTest},
-    packages=['dkmodelfields'],
     zip_safe=False,
 )
